d1251e31847a48cf6bd97465dc431345267e328c
Henrik Muehe <henrik.muehe@gmail.com>
2015-01-22 20:51:25 -0800

    adjusted rounds

--- loops/insn.c
+++ loops/insn.c
@@ -3,19 +3,19 @@
 
 __attribute__((noinline))
 void insn_down(int argc) {
-  const int64_t l=1000000000ll+argc;
+  const int64_t l=100000000ll+argc;
   for (int64_t i=l; i!=0; --i) asm volatile("nop"::"r"(i));
 }
 
 __attribute__((noinline))
 void insn_up_lt(int argc) {
-  const int64_t l=1000000000ll+argc;
+  const int64_t l=100000000ll+argc;
     for (int64_t i=0; i < l; ++i) asm volatile ("nop"::"r"(i));
 }
 
 __attribute__((noinline))
 void insn_up_eq(int argc) {
-  const int64_t l=1000000000ll+argc;
+  const int64_t l=100000000ll+argc;
     for (int64_t i=0; i!=l; ++i) asm volatile ("nop"::"r"(i));
 }
 
